aa6bd5770323dc3cae6f043b42f31aa3cd875a6c
minyiky <mmcdonnell@hotmail.co.uk>
2022-12-03 11:16:07 +0000

    Added test pass status

---
+++ 2022/script/test_badge_go.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+num_pass=$(go test -v ./... | grep -c PASS:)
+num_tests=$(grep -r --exclude-dir=script "func Test" . | wc -l)
+
+perc_passed=$(expr $num_pass / $num_tests)
+
+if [ $perc_passed = 1 ]; then
+    colour=success
+elif [ $perc_passed > 0.5 ]; then
+    colour=important
+else
+    colour=critical
+fi
+
+sed -i "s/[0-9]\+\/[0-9]\+-[a-z]\+/$num_pass\/$num_tests-$colour/" README.md
