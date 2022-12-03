40a6eb5d6c9bd59ae5b10acb503c1dfd99c74a6f
minyiky <mmcdonnell@hotmail.co.uk>
2022-12-03 16:17:58 +0000

    better command for touching files

--- 2022/script/test_badge_go.sh
+++ 2022/script/test_badge_go.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
- touch ./day{0,1,2}{0,1,2,3,4,5,6,7,8,9}/input.txt
+find ./day* -type d -exec touch {}/input.txt \;
 
 num_pass=$(go test -v ./... | grep -c PASS:)
 num_tests=$(grep -r --exclude-dir=script "func Test" . | wc -l)
