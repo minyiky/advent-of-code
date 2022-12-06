0314110a652a080bfb592afa29a19b87163818bf
minyiky <minyiky@users.noreply.github.com>
2022-12-06 02:12:46 +0000

    Generated files for the new day

---
+++ 2022/day06/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/minyiky/advent-of-code/2022/day06"
+
+func main() {
+	day06.Run()
+}
---
+++ 2022/day06/part1.go
@@ -0,0 +1,23 @@
+package day06
+
+import (
+	"log"
+)
+
+func Part1Val(lines []string) (int, error) {
+	var value int
+
+	for _, line := range lines{
+	}
+
+	return value, nil
+}
+
+func Part1(lines []string) error {
+	value, err := Part1Val(lines)
+	if err != nil {
+		return err
+	}
+	log.Printf("The value found was: %d", value)
+	return nil
+}
