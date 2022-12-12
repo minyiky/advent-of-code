5ed5e3ad6ef75c0d2fc95e7f44d71feb33e47ba1
minyiky <minyiky@users.noreply.github.com>
2022-12-12 02:15:37 +0000

    Generated files for the new day

---
+++ 2022/day12/part2.go
@@ -0,0 +1,24 @@
+package day12
+
+import (
+	"log"
+)
+
+func Part2Val(lines []string) (int, error) {
+	var value int
+
+	for _, line := range lines{
+		_ = line
+	}
+
+	return value, nil
+}
+
+func Part2(w io.Writer, lines []string) error {
+	value, err := Part2Val(lines)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "The value found was: %d\n", value)
+	return nil
+}
