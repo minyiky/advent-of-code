390c04cc45fe80d7655b8a0dd17bb5cf12bd1d43
minyiky <minyiky@users.noreply.github.com>
2022-12-13 02:20:21 +0000

    Generated files for the new day

---
+++ 2022/day13/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/minyiky/advent-of-code/2022/day13"
+
+func main() {
+	day13.Run(os.Stdout)
+}
---
+++ 2022/day13/day13_test.go
@@ -0,0 +1,39 @@
+package day13_test
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+
+	day "github.com/minyiky/advent-of-code/2022/day13"
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed input_test.txt
+var input string
+
+func SetUp() []string {
+	input = strings.ReplaceAll(input, "\r", "")
+	lines := strings.Split(input, "\n")
+	return lines
+}
+
+func Test_Part1(t *testing.T) {
+	lines := SetUp()
+	expected := 0
+
+	val, err := day.Part1Val(lines)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, val)
+}
+
+func Test_Part2(t *testing.T) {
+	lines := SetUp()
+	expected := 0
+
+	val, err := day.Part2Val(lines)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, val)
+}
