40be80a1fa9df981c0624fae76f76163b5700599
minyiky <minyiky@users.noreply.github.com>
2022-12-24 01:59:58 +0000

    Generated files for the new day

---
+++ 2022/day24/cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"os"
+
+	"github.com/minyiky/advent-of-code/2022/day24"
+)
+
+func main() {
+	day24.Run(os.Stdout)
+}
---
+++ 2022/day24/day24.go
@@ -0,0 +1,31 @@
+package day24
+
+import (
+	_ "embed"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func Run(w io.Writer) {
+	input = strings.ReplaceAll(input, "\r", "")
+	lines := strings.Split(input, "\n")
+
+	fmt.Fprintf(w, "-- Solution for 2022 day 24 --\n")
+	if err := Part1(w , lines); err != nil {
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+	}
+	if err := Part2(w , lines); err != nil {
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+	}
+}
