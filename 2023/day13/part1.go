bfd26e29d0f643afa9e8e327b098a7713e77a714
minyiky <mmcdonnell@hotmail.co.uk>
2023-12-13 23:07:37 +0000

    solution for day 13

--- 2023/day13/part1.go
+++ 2023/day13/part1.go
@@ -6,12 +6,58 @@ import (
 	"time"
 )
 
+func calcBlock(block []string) int {
+	calc := func(block []string) int {
+	loop:
+		for i := range block[:len(block)-1] {
+			// fmt.Println()
+			for j := 0; j < len(block)-1; j++ {
+				if i-j < 0 || i+j+1 == len(block) {
+					// fmt.Println(i, j, "breaking")
+					break
+				}
+				// fmt.Println(j, block[i-j])
+				// fmt.Println(j, block[j+i+1])
+				if block[i-j] != block[j+1+i] {
+					continue loop
+				}
+			}
+			// fmt.Println(i)
+			return i + 1
+		}
+		return 0
+	}
+
+	if v := calc(block); v != 0 {
+		return 100 * v
+	}
+
+	newBlock := make([]string, 0, len(block[0]))
+
+	for i := range block[0] {
+		l := ""
+		for _, line := range block {
+			l += line[i : i+1]
+		}
+		newBlock = append(newBlock, l)
+	}
+
+	return calc(newBlock)
+}
+
 func Part1Val(lines []string) (int, error) {
 	var value int
 
-	for _, line := range lines{
-		_ = line
+	block := make([]string, 0)
+	for _, line := range lines {
+		if line == "" {
+			value += calcBlock(block)
+			block = make([]string, 0)
+			continue
+		}
+		block = append(block, line)
 	}
+	value += calcBlock(block)
 
 	return value, nil
 }
--- 2023/day13/part2.go
+++ 2023/day13/part2.go
@@ -6,12 +6,64 @@ import (
 	"time"
 )
 
+func calcBlockSmudge(block []string) int {
+	calc := func(block []string) int {
+		for i := range block[:len(block)-1] {
+			diffs := 0
+			// fmt.Println()
+			for j := 0; j < len(block)-1; j++ {
+				if i-j < 0 || i+j+1 == len(block) {
+					// fmt.Println(i, j, "breaking")
+					break
+				}
+				// fmt.Println(j, block[i-j])
+				// fmt.Println(j, block[j+i+1])
+				upper := block[i-j]
+				lower := block[i+j+1]
+				for k := range block[0] {
+					if upper[k] != lower[k] {
+						diffs++
+					}
+				}
+			}
+			// fmt.Println(i)
+			if diffs == 1 {
+				return i + 1
+			}
+		}
+		return 0
+	}
+
+	if v := calc(block); v != 0 {
+		return 100 * v
+	}
+
+	newBlock := make([]string, 0, len(block[0]))
+
+	for i := range block[0] {
+		l := ""
+		for _, line := range block {
+			l += line[i : i+1]
+		}
+		newBlock = append(newBlock, l)
+	}
+
+	return calc(newBlock)
+}
+
 func Part2Val(lines []string) (int, error) {
 	var value int
 
-	for _, line := range lines{
-		_ = line
+	block := make([]string, 0)
+	for _, line := range lines {
+		if line == "" {
+			value += calcBlockSmudge(block)
+			block = make([]string, 0)
+			continue
+		}
+		block = append(block, line)
 	}
+	value += calcBlockSmudge(block)
 
 	return value, nil
 }
