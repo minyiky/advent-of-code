7cbf92079b897a601d563a897c13042bd097f3c6
minyiky <minyiky@users.noreply.github.com>
2025-12-12 06:32:56 +0000

    Generated files for the new day

---
+++ 2025/day12/cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"os"
+
+	"github.com/minyiky/advent-of-code/2025/day12"
+)
+
+func main() {
+	day12.Run(os.Stdout)
+}
---
+++ 2025/day12/part1.go
@@ -0,0 +1,29 @@
+package day12
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+func Part1Val(lines []string) (int, error) {
+	var value int
+
+	for _, line := range lines{
+		_ = line
+	}
+
+	return value, nil
+}
+
+func Part1(w io.Writer, lines []string) error {
+	start := time.Now()
+	value, err := Part1Val(lines)
+	if err != nil {
+		return err
+	}
+	duration := time.Since(start)
+	fmt.Fprintf(w, "The value found was: %d\n", value)
+	fmt.Fprintf(w, "This took %.2fms\n", float64(duration)/1e6)
+	return nil
+}
