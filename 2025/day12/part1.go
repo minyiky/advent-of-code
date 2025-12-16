5ed910fb619d44edf80aaad8c68b1e71d476428e
Michael McDonnell <mmcdonnell@hotmail.co.uk>
2025-12-16 13:58:02 +0000

    solution for day 12

--- 2025/day12/part1.go
+++ 2025/day12/part1.go
@@ -3,14 +3,43 @@ package day12
 import (
 	"fmt"
 	"io"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func Part1Val(lines []string) (int, error) {
 	var value int
 
-	for _, line := range lines{
-		_ = line
+	for _, line := range lines {
+		size, blockSet, found := strings.Cut(line, ":")
+		if !found {
+			continue
+		}
+
+		var w, h int
+		_, err := fmt.Sscanf(size, "%dx%d", &w, &h)
+		if err != nil {
+			continue
+		}
+
+		w = (w / 3)
+		h = (h / 3)
+
+		blocks := strings.Fields(blockSet)
+		area := w * h
+		blockArea := 0
+		for _, block := range blocks {
+			numBlocks, err := strconv.Atoi(block)
+			if err != nil {
+				continue
+			}
+			blockArea += numBlocks
+		}
+
+		if area >= blockArea {
+			value += 1
+		}
 	}
 
 	return value, nil
